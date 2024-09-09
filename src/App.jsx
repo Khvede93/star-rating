8ad50122352534d2fa063857f49565dbe4e819e2
Khvede93 <gio.khvede93@gmail.com>
2024-09-10 01:10:55 +0200

    add temp structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { FaStar } from 'react-icons/fa';
 import './App.css';
 
 function App() {
-  return <div>Star Rating</div>;
+  function handleClick() {}
+  function handleMouseOver() {}
+  function handleMouseLeave() {}
+
+  const numberOfStars = 10;
+  return (
+    <div className='star-rating'>
+      {[...Array(numberOfStars)].map((_, i) => (
+        <FaStar
+          key={i}
+          onClick={handleClick}
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          size={40}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
