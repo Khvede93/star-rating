4fdca9bbb65d53eaac5d944a653867c1e50b4a4c
Khvede93 <gio.khvede93@gmail.com>
2024-09-10 01:33:28 +0200

    star rating project is done

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,18 @@ function App() {
   const [hover, setHover] = useState(0);
 
   function handleClick(getInd) {
-    if (getInd !== rating) {
-      setRating(getInd);
-    }
+    setRating(getInd);
   }
   function handleMouseOver(getInd) {
-    if (getInd !== hover) {
-      setHover(getInd);
-    }
+    setHover(getInd);
   }
-  function handleMouseLeave(getInd) {
-    setHover(0);
+  function handleMouseLeave() {
+    setHover(rating);
   }
 
   const numberOfStars = 10;
   return (
-    <div className='star-rating'>
+    <div className='starRating'>
       {[...Array(numberOfStars)].map((_, ind) => {
         ind += 1;
         return (
@@ -30,9 +26,9 @@ function App() {
             key={ind}
             onClick={() => handleClick(ind)}
             onMouseEnter={() => handleMouseOver(ind)}
-            onMouseLeave={() => handleMouseLeave(ind)}
+            onMouseLeave={handleMouseLeave}
             size={40}
-            className={ind <= (rating || hover) ? 'active' : 'inActive'}
+            className={ind <= (hover || rating) ? 'active' : 'inActive'}
           />
         );
       })}
