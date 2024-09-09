2720f0ed1369f34b84a70a752f648f59f0f82ee9
Khvede93 <gio.khvede93@gmail.com>
2024-09-10 01:15:50 +0200

    rating onclick working version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { FaStar } from 'react-icons/fa';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  function handleClick() {}
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getI) {
+    if (getI !== rating) {
+      setRating(getI + 1);
+    }
+  }
   function handleMouseOver() {}
   function handleMouseLeave() {}
+
+  console.log(rating);
 
   const numberOfStars = 10;
   return (
@@ -12,7 +22,7 @@ function App() {
       {[...Array(numberOfStars)].map((_, i) => (
         <FaStar
           key={i}
-          onClick={handleClick}
+          onClick={() => handleClick(i)}
           onMouseEnter={handleMouseOver}
           onMouseLeave={handleMouseLeave}
           size={40}
